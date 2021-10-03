98a98b50f5a033e5e0e079510db2cd084408d85d
Matt McElwee <mmcelwee@dmsi.com>
2021-10-03 07:25:18 -0500

    Completed the auth flow

--- functions/auth.ts
+++ functions/auth.ts
@@ -1,10 +1,46 @@
 import { Handler } from '@netlify/functions';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import cookie from 'cookie';
+
+const EMAIL_SECRET = process.env.EMAIL_TOKEN;
+const COOKIE_SECRET = process.env.COOKIE_TOKEN;
+
+const safelyVerify = (token: string) => {
+  try {
+    return jwt.verify(token, EMAIL_SECRET);
+  } catch (e) {
+    return null;
+  }
+};
 
 const handler: Handler = async (event, context) => {
   const { token } = event.queryStringParameters;
 
+  const verified = await safelyVerify(token);
+
+  if (!verified) {
+    return Promise.resolve({
+      statusCode: 301,
+      headers: {
+        Location: '/',
+      },
+    });
+  }
+
+  const { email } = verified as JwtPayload;
+
+  const cookieToken = await jwt.sign({ email }, COOKIE_SECRET);
+
   return {
-    statusCode: 200,
+    headers: {
+      'set-cookie': cookie.serialize('babylon-token', cookieToken, {
+        maxAge: 60 * 60 * 24 * 7 * 7, // 7 weeks
+        httpOnly: true,
+        domain: event.headers.host,
+      }),
+      Location: '/',
+    },
+    statusCode: 301,
   };
 };
 
