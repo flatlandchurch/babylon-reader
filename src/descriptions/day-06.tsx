db3b76f70d75512dfb3779eae85bfa1b9ed0bf02
Matt McElwee <mmcelwee@dmsi.com>
2021-10-07 20:07:14 -0500

    Added a description for day 6

--- src/descriptions/day-06.tsx
+++ src/descriptions/day-06.tsx
@@ -2,7 +2,33 @@ import { h } from 'preact';
 import { Paragraph } from './styles';
 
 const Day = () => {
-  return <Paragraph />;
+  return (
+    <Paragraph>
+      <h2>Intro</h2>
+      <p>
+        The first blessing God gave humanity was to be fruitful and multiply. Then he blessed
+        Abraham and made a covenant with him, saying that he would become a great nation and that
+        his offspring would be come so numerous you could hardly count them.
+      </p>
+      <p>
+        At the start of Exodus 1, we see the blessing of God manifest in the people of Israel. They
+        were <em>exceedingly</em> fruitful and multiplied. But the king of Egypt saw this not as a
+        blessing, but as a threat. The way of Babylon is one characterized by scarcity. The king of
+        Egypt fears that the Israelites might become rebels because they outnumber the Egyptians, so
+        they enslave them and oppress them with heavy labor and with violence.
+      </p>
+      <p>
+        The culture of Egyptian slavery is one in which every day is filled with wearisome labor and
+        heavy burdens. And even when begged to allow the people to rest for just three days, the
+        king declares that the people are lazy and must be worked harder.
+      </p>
+      <p>
+        But YHWH hears the cries of his people Egypt, just as he heard the cries of Abel's blood, of
+        the violence the humans did before the flood, of the oppressive tower building in Babylon,
+        of the city of Sodom, and of the poor foreigner, Hagar. God is a god who hears.
+      </p>
+    </Paragraph>
+  );
 };
 
 export default Day;
