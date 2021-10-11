c7f4da81a2ad539d08bfa03a50eb75c59da9db24
Matt McElwee <mmcelwee@dmsi.com>
2021-10-10 19:00:25 -0500

    Fixed a bug in ketchup:

--- src/conditions/ketchup.test.ts
+++ src/conditions/ketchup.test.ts
@@ -23,24 +23,36 @@ test('ketchup - when two readings are done on the same day, return true', (t) =>
 test('ketchup - when no readings are done on the same day, return false', (t) => {
   const days = [
     {
-      read: '2021-10-04T02:16:46.441Z',
+      read: '2021-10-04T01:41:28.194Z',
       day: 1,
     },
     {
-      read: '2021-10-05T03:11:23.773Z',
+      read: '2021-10-04T05:21:44.107Z',
       day: 2,
     },
     {
-      read: '2021-10-06T03:51:19.684Z',
+      read: '2021-10-05T23:29:34.685Z',
       day: 3,
     },
     {
-      read: '2021-10-07T03:44:39.109Z',
+      read: '2021-10-06T16:17:01.038Z',
       day: 4,
     },
     {
-      read: '2021-10-08T03:40:40.199Z',
+      read: '2021-10-07T07:02:47.479Z',
       day: 5,
+    },
+    {
+      read: '2021-10-08T10:29:37.705Z',
+      day: 6,
+    },
+    {
+      read: '2021-10-09T06:17:25.687Z',
+      day: 7,
+    },
+    {
+      read: '2021-10-10T09:29:20.178Z',
+      day: 8,
     },
   ];
 
--- src/conditions/ketchup.ts
+++ src/conditions/ketchup.ts
@@ -4,7 +4,7 @@ import utcToJSDate from './utils/utcToJSDate';
 const ketchup = (days: Completion[]): boolean => {
   const dayMap = days.reduce((acc: { [key: string]: number }, day) => {
     const date = utcToJSDate(day.read);
-    const key = `${date.getMonth() + 1}-${date.getDay()}`;
+    const key = `${date.getMonth() + 1}-${date.getDate()}`;
     acc[key] = !acc[key] ? 1 : acc[key] + 1;
     return acc;
   }, {});
