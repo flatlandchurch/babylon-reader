48c9bd76d20756f731932b3a63671e6bca6ff70b
Matt McElwee <mmcelwee@dmsi.com>
2021-10-10 22:03:34 -0500

    Added description for day 9 - Once again changed the start badge colors - Added a badge for torah reading

--- src/descriptions/day-09.tsx
+++ src/descriptions/day-09.tsx
@@ -2,7 +2,35 @@ import { h } from 'preact';
 import { Paragraph } from './styles';
 
 const Day = () => {
-  return <Paragraph />;
+  return (
+    <Paragraph>
+      <h2>Intro</h2>
+      <p>
+        God institutes a covenant with the people of Israel from Mount Sinai, which we read the
+        opening words of yesterday. The covenant is focused primarily on God being the ruler of the
+        Israelite nation, with priests, prophets, and kings as representatives of his rulership and
+        with the nation of Israel serving as representatives of God to the nations.
+      </p>
+      <p>
+        But in addition, God is outlining how the people of Israel are meant to be a new kind of
+        people. A nation that isn't anything like Babylon or Egypt. In Leviticus 16 we see God
+        addressing the problem of sin, not through vengeance, but through atoning sacrifice through
+        an animal. In Leviticus 23, he outlines a number of festivals in which the people remember
+        that they belong to God and they rest in the goodness of the land God gives them.
+      </p>
+      <p>
+        In Deuteronomy 17, God outlines what type of king Israel should have. This sort of king must
+        not acquire much gold and silver, nor accumulate horses (especially when those horses come
+        from Egypt). And he mustn't accumulate many wives lest they lead him into idolatry. This is
+        filled with hyperlinks forward, so pay attention to the description of the king here,
+        because you'll need it later.
+      </p>
+      <p>
+        Finally, Deuteronomy 28 prepares the people of Israel for what breaking their side of the
+        agreement with God will look like: exile.
+      </p>
+    </Paragraph>
+  );
 };
 
 export default Day;
