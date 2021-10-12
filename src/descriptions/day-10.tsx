372d0a94fab2ce691e3afa1d200cc35858111298
Matt McElwee <mmcelwee@dmsi.com>
2021-10-11 21:53:58 -0500

    Added an intro for day 9

--- src/descriptions/day-10.tsx
+++ src/descriptions/day-10.tsx
@@ -2,7 +2,31 @@ import { h } from 'preact';
 import { Paragraph } from './styles';
 
 const Day = () => {
-  return <Paragraph />;
+  return (
+    <Paragraph>
+      <h2>Intro</h2>
+      <p>
+        Judges 1 and 2 paint a stark picture of the trajectory of Israel. While things begin
+        positively with great military victory, things begin to spiral out of control. While the
+        Israelites were meant to completely drive out the Canaanites from their lands, many of the
+        tribes allowed to live the foreign nations remain, mixing with them, taking on their customs
+        and their gods.
+      </p>
+      <p>
+        But as we remember from yesterday in Deuteronomy 28, God expected covenant faithfulness from
+        the Israelites. The consequence would be the incursion of foreign military powers, and
+        eventually, exile. Most of the book of Judges tracks the downward descent of Israel from the
+        victories of Joshua to a nation which is indistinguishable – if not entirely worse – than
+        the Canaanite nations around them.
+      </p>
+      <p>
+        Judges 17 begins a truly gut-wrenching tale of a family trying to define good and evil for
+        themselves (a hyperlink back to Genesis 3), creating an idol from personal wealth (a
+        hyperlink back to Exodus 32), and distorting the holiness and singularity of the Tabernacle
+        worship and practices.
+      </p>
+    </Paragraph>
+  );
 };
 
 export default Day;
