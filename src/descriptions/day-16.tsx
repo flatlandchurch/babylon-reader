4e5d9b99a123012e0b67b6fce767e3295504e0c5
Matt McElwee <mmcelwee@dmsi.com>
2021-10-17 22:12:12 -0500

    Added a description for day 16

--- src/descriptions/day-16.tsx
+++ src/descriptions/day-16.tsx
@@ -5,6 +5,27 @@ const Day = () => {
   return (
     <Paragraph>
       <h2>Intro</h2>
+      <p>
+        We're going back to Deuteronomy, because there's something important we need to see there.
+        In Deuteronomy 20, we see a list of people exempted from participating in war. God says that
+        if you have recently become betrothed you must not go to war, rather, they should marry. And
+        if you've just bought a house, you must not go to war, rather, enjoy your new home. And if
+        you planted a vineyard, you should wait until you can enjoy the fruits of the vineyard
+        before going to war.
+      </p>
+      <p>
+        What does that have to do with exile? Well just before the exile, the big event we read
+        about yesterday in 2 Kings 25, there were two streams of thought among Israel's prophets.
+        One side, led by Hananiah, believed that Israel needed to take up arms and overthrow the
+        Babylonian oppressors (Jeremiah 28). But the other side, which we learn was the side God was
+        on the whole time, believed that the people of Israel needed to seek the peace of Babylon.
+      </p>
+      <p>
+        Jeremiah, the prophet delivering this message, used an interesting phrase to describe this
+        peace: plant vineyards, build houses, and get married. This is a direct hyperlink back to
+        Deuteronomy 20. Jeremiah uses the language of rest and exemption from war to call all of
+        God's people to peace over violent resistance.
+      </p>
     </Paragraph>
   );
 };
