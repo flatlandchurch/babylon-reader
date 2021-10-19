b0b006d2f85288314ff311a51b8a7f7703a22585
Matt McElwee <mmcelwee@dmsi.com>
2021-10-18 23:06:48 -0500

    Added an intro for day 17

--- src/descriptions/day-17.tsx
+++ src/descriptions/day-17.tsx
@@ -5,6 +5,20 @@ const Day = () => {
   return (
     <Paragraph>
       <h2>Intro</h2>
+      <p>
+        In yesterday's reading, Jeremiah told the people of Israel on their way to exile to seek the
+        prosperity of the city in which they dwelled. Daniel and his friends find themselves in the
+        heart of the Babylonian empire and they are constantly faced with what seems like only two
+        ways: be a peacemaker and conform to Babylonian customs, worship, and practice, or resist
+        Babylon through vocal uprising, mockery, and violence.
+      </p>
+      <p>
+        But Daniel and his friends discover a middle way. A way that is characterized by radical
+        faithfulness to God and his instruction, but that is also peaceable and seeks the good of
+        neighbor. Like we will eventually see with Jesus, this middle way often comes at great
+        personal sacrifice. Daniel and his friends risk status, well-being, and even their lives in
+        order to exercise peaceful, subversive resistance.
+      </p>
     </Paragraph>
   );
 };
