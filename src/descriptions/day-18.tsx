a02b877eebe727071e1ab9e1a4d42721b1800cc0
Matt McElwee <mmcelwee@dmsi.com>
2021-10-19 22:55:50 -0500

    Added an intro for day 18

--- src/descriptions/day-18.tsx
+++ src/descriptions/day-18.tsx
@@ -5,6 +5,22 @@ const Day = () => {
   return (
     <Paragraph>
       <h2>Intro</h2>
+      <p>
+        Today's stories are among the most popular in the book of Daniel. Once again, we see the
+        faithfulness of God's people is met with divine protection. We also read a very interesting
+        tale about a king who is given authority and __dominion__ over all the world. But he also
+        became proud. A Babylonian king who had grown proud of his own achievements, having made a
+        name for himself. Now that sounds familiar.
+      </p>
+      <p>
+        God's response is to curse the king to be like a beast, driven out to the fields until he
+        can acknowledge God's greatness. Once again we see the inversion of rulership established in
+        Genesis 1, as a human becomes like the beasts they were meant to rule.
+      </p>
+      <p>
+        As you read chapter 6, ponder how it connects to other stories you've read so far. And think
+        about the relationship of the faithful person of God and the beasts.
+      </p>
     </Paragraph>
   );
 };
