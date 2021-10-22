c423bd8511b193eb9ea3a1fb805f1f95e2ec741c
Matt McElwee <mmcelwee@dmsi.com>
2021-10-22 09:54:47 -0500

    Added a (late) intro to day 20

--- src/descriptions/day-20.tsx
+++ src/descriptions/day-20.tsx
@@ -5,6 +5,18 @@ const Day = () => {
   return (
     <Paragraph>
       <h2>Intro</h2>
+      <p>
+        Daniel 9 contains a prayer which is so deeply saturated in the rest of the Hebrew Bible (and
+        coincidentally, to our readings over the last three weeks). Daniel realizes, having read the
+        letter of Jeremiah (Jeremiah 29), the exile must soon be over. Take time and think about the
+        readings you've already done this series and notice how they are brought up in prayer by
+        Daniel.
+      </p>
+      <p>
+        Daniel 10 and 11 set up an end of the world, global-scale conflict which will ultimately be
+        paid off in the Book of Revelation. Much of the imagery in these two chapters will be
+        important later, so take inventory of the language used throughout.
+      </p>
     </Paragraph>
   );
 };
