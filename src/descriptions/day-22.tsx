802a01c8b7eb4a8fb768331beda16b33d9bdee39
Matt McElwee <mmcelwee@dmsi.com>
2021-10-23 22:35:05 -0500

    Added intro for tomorrow

--- src/descriptions/day-22.tsx
+++ src/descriptions/day-22.tsx
@@ -5,6 +5,29 @@ const Day = () => {
   return (
     <Paragraph>
       <h2>Intro</h2>
+      <p>
+        When Daniel read the scroll of Jeremiah, he realized that the time of the exile was well
+        past due. When he questioned the angel about this time table, the angel revealed that even
+        upon returning home, the people of Israel would still experience something of a spiritual
+        exile.
+      </p>
+      <p>
+        When the Hebrew Bible ends, Cyrus has just allowed the Jews to return to Judah and
+        reestablish their lives, kingdom, and Temple. Over the next nearly 5 centuries, the people
+        of Israel experienced the constant cycle of empire. Every time a new empire would rise to
+        world-wide dominance, another would come up from beneath it, crush it, and start the cycle
+        again.
+      </p>
+      <p>
+        Meanwhile, the Jewish people experienced profound spiritual renewal, seeking a new way to
+        follow God focused on covenant faithfulness and avoiding a return to exile.
+      </p>
+      <p>
+        Yet just as in the days of old, the kings of Israel often become more like Pharaoh than the
+        prophet-priest-kings of Deuteronomy 17. And just like Pharaoh went after all the baby boys
+        in Exodus 1, King Herod seeks the lives of the baby boys in Matthew 2. But in both cases,
+        God's chosen deliverer is saved through the nation of Egypt.
+      </p>
     </Paragraph>
   );
 };
