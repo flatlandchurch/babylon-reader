4c3bb7b70e5e38fcb8f9de8ccf52093bfdeefc93
Matt McElwee <mmcelwee@dmsi.com>
2021-10-24 23:17:30 -0500

    Added an intro for day 23

--- src/descriptions/day-23.tsx
+++ src/descriptions/day-23.tsx
@@ -5,6 +5,18 @@ const Day = () => {
   return (
     <Paragraph>
       <h2>Intro</h2>
+      <p>
+        The Book of Mark is the most concise of the four Gospel accounts. It has a pretty simple
+        aim, to demonstrate that Jesus is the Son of God. While each of the Gospel accounts are
+        filled with hyperlinks, Mark moves from beginning to end with little reflection or side
+        paths.
+      </p>
+      <p>
+        In today's reading alone, we'll see references to the Genesis 1, Exodus, the Day of
+        Atonement, and Daniel 7. We'll begin to see the culmination of the readings of the last
+        three weeks come to a head this week as the hyperlinks are layered one after another to
+        demonstrate the Bible as a unified story that leads to Jesus.
+      </p>
     </Paragraph>
   );
 };
