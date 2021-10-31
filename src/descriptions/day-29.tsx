0d172f25cbd9ecc46b972185fa89d4dbbc787219
Matt McElwee <mmcelwee@dmsi.com>
2021-10-30 22:52:33 -0400

    Added an intro for day 29

--- src/descriptions/day-29.tsx
+++ src/descriptions/day-29.tsx
@@ -5,6 +5,11 @@ const Day = () => {
   return (
     <Paragraph>
       <h2>Intro</h2>
+      <p>
+        The Church is one of God's instruments for giving grace and hope to the world. But the
+        Church often finds herself living in Babylon. Peter develops the metaphor and archetype of
+        Babylon throughout his letter to encourage the Church to perseverance and faithfulness.
+      </p>
     </Paragraph>
   );
 };
